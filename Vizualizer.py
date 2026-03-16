28d8e2d91eef018326f0085e722590bc03018714
yainge <yainge@gmail.com>
2026-03-16 18:37:08 +0100

    Update to centralized control logic

--- Vizualizer.py
+++ Vizualizer.py
@@ -28,8 +28,8 @@ class Vizualizer:
         plt.legend()
         plt.grid(True)
         plt.savefig(f"figures/{self.mode}_total_load.png", dpi=150, bbox_inches="tight")
-        plt.show()
-        plt.close()
+        # plt.show()
+        # plt.close()
 
 
         # Calculate average daily profile
@@ -53,8 +53,8 @@ class Vizualizer:
         plt.legend()
         plt.grid(True)
         plt.savefig(f"figures/{self.mode}_daily_profile.png", dpi=150, bbox_inches="tight")
-        plt.show()
-        plt.close()
+        #plt.show()
+        #plt.close()
 
     def print_metrics_renewable_share_total_load(self, renewable_share : np.ndarray, total_load : np.ndarray):
         """
